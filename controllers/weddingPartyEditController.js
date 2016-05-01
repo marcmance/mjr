16035c182844e25064e2331e875bcfb455ff663c
Mance <marc.mance@gmail.com>
2016-05-01 00:24:44 -0400

    save and edit

--- controllers/weddingPartyEditController.js
+++ controllers/weddingPartyEditController.js
@@ -3,6 +3,13 @@ mjr.controller('weddingPartyEditController', ['$scope', '$route', '$rootScope', 
 
   var ref = new Firebase("https://marcandjennyromance.firebaseio.com/copy/wp/");
   $scope.weddingparty = $firebaseArray(ref);
-
+  console.log($scope.weddingparty);
+  $scope.save = function() {
+    for(var x = 0; x < 6; x++) {
+      var id = $scope.weddingparty[x].$id;
+      var why = $scope.weddingparty.$getRecord(id);
+      $scope.weddingparty.$save(why);
+    }
+  }
 
 }]);
