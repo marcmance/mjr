764710f97c477430857bd0329de13dc19b5aedb2
Mance <marc.mance@gmail.com>
2016-05-25 21:25:56 -0400

    cache bust

--- mjr.js
+++ mjr.js
@@ -4,5 +4,11 @@ var mjr = angular.module('mjr', ['ngResource', 'ngRoute', 'ngSanitize','firebase
   function ($rootScope) {
     console.log("9/10/16");
     $rootScope.nonHome = false;
+
+    for (var key in window.localStorage) {
+      if (key.indexOf('http://marcandjennyromance.com/css/main.less') === 0) {
+        delete window.localStorage[key];
+      }
+    }
   }
 ]);
